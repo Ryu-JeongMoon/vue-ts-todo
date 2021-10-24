e3e4efff69784a4a4b0a5c86d1c34fdfdcef3764
jm <rjm9303@gmail.com>
2021-10-24 17:53:27 +0900

    조회 기능

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <header>
+      <h1>Vue Todo with Typescript</h1>
+    </header>
+    <main>
+      <TodoInput
+        :item="todoText"
+        @input="updateTodoText"
+        @add="addTodoText"
+      ></TodoInput>
+      <ul>
+        <TodoListItem
+          v-for="(todoItem, index) in todoItems"
+          :key="index"
+          :todoItem="todoItem"
+        ></TodoListItem>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoInput from "@/components/TodoInput.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItems: any[]) {
+    const parsed = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+  fetch() {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
 
 export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld,
+  components: { TodoListItem, TodoInput },
+  data() {
+    return {
+      todoText: "",
+      todoItems: [] as any[],
+    };
+  },
+  methods: {
+    updateTodoText(value: string) {
+      this.todoText = value;
+    },
+    addTodoText() {
+      const value = this.todoText;
+      this.todoItems.push(value);
+      storage.save(this.todoItems);
+      this.initTodoText();
+    },
+    initTodoText() {
+      this.todoText = "";
+    },
+    fetchTodoItems() {
+      this.todoItems = storage.fetch();
+    },
+  },
+  created() {
+    this.fetchTodoItems();
   },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
